63f881e914d144d812ed3f1ddd3f4e7ad762fbad
9GAG <sonalanand102@gmail.com>
2023-02-02 23:43:23 +0530

    homepage added

--- src/api/servicesApi.js
+++ src/api/servicesApi.js
@@ -1,26 +1,28 @@
 const servicesApi = [
     {
         id : 1,
-        service_img : '',
-        name : '',
-        desc : ''
+        service_img : require('../images/opthamology-service.png'),
+        name : 'Ophthalmology',
+        desc : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
     },
     {
         id : 2,
-        service_img : '',
-        name : '',
-        desc : ''
+        service_img : require('../images/gastroentrology-service.png'),
+        name : 'Gastroenterology',
+        desc : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
     },
     {
         id : 3,
-        service_img : '',
-        name : '',
-        desc : ''
+        service_img : require('../images/internal-medicine-service.png'),
+        name : 'Internal Medicine',
+        desc : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
     },
     {
         id : 4,
-        service_img : '',
-        name : '',
-        desc : ''
+        service_img : require('../images/physiotherpy-service.png'),
+        name : 'Physiotherapy',
+        desc : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
     },
 ]
+
+module.exports =  servicesApi;
